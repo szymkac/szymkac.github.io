da75f78de123a079a4c5815d7c0607379d373476
Szymek <Szymek1295@gmail.com>
2018-06-14 15:48:40 +0200

    Finished front

--- js/applicationFunctionality.js
+++ js/applicationFunctionality.js
@@ -1,46 +1,121 @@
 var currentDate = null;
-var amountOfCycles = 5;
+var amountOfCycles = 10;
 var cycles = 1;
 var timeDifference = [];
+var audio = null;
+var gameState = "INTRODUCTION";
+var gameStages = 3;
+var results = {};
+var actualGameStage = 0;
+var counting = false;
 
 $(function () {
     var x = document.getElementById("moving-block");
-    x.addEventListener("webkitAnimationIteration", animStart);
-    x.addEventListener("animationIteration", animStart);
+    x.addEventListener("webkitAnimationIteration", animIteration);
+    x.addEventListener("animationIteration", animIteration);
 })
 
 $(document).keypress(function (event) {
     if (event.which == 32) {
-        var date = new Date();
-        if (currentDate && cycles < amountOfCycles + 1 && timeDifference.length < cycles) {
-            var text = $('#result-container').html();
-            $('#result-container').html(text + "<span class='red'>" + date + "</span><br/>" + "<span class='green'>" + Math.abs(date - currentDate) + "</span><br/>");
-            timeDifference.push(Math.abs(date - currentDate));
-        }
-        else if (cycles === 1) {
-            $('#moving-block').css("animation-play-state", "running")
-            currentDate = date;
-            $('#result-container').html("<span>" + date + "</span><br/>");
+        if (!counting) {
+            switch (gameState) {
+                case "INTRODUCTION":
+                    gameState = "GAME";
+                    $('#message-container').html("Naciśnij spację, kiedy wskaźnik znajdzie się na środku paska.")
+                    countTo(3);
+                    break;
+                case "BREAK":
+                    gameState = "GAME";
+                    $('#message-container').html("Naciśnij spację, kiedy wskaźnik znajdzie się na środku paska.")
+                    audioStart(actualGameStage);
+                    countTo(3);
+                    break;
+                case "GAME":
+                    gameCycle();
+                    break;
+            }
         }
     }
 });
 
-function animStart() {
-    var text = $('#result-container').html();
+function gameCycle() {
+    var date = new Date();
+    if (currentDate && cycles < amountOfCycles + 1 && timeDifference.length < cycles) {
+        timeDifference.push(Math.abs(date - currentDate));
+        $('#counter-container').empty();
+    }
+}
+function gameStart() {
+    var date = new Date();
+    $('#moving-block').css("animation-play-state", "running")
+    currentDate = date;
+    actualGameStage++;
+}
+function animIteration() {
     var date = new Date();
     currentDate = date;
-    
+
     if (timeDifference.length < cycles)
         timeDifference.push(null);
 
     cycles++;
-    
-    if (cycles < amountOfCycles + 1){
-        $('#result-container').html(text + "<span>" + date + "</span><br/>");
-        $('#cycles-container').html(cycles);
+
+    if (cycles > amountOfCycles) {
+        $('#moving-block').css("animation-play-state", "paused")
+        console.log(timeDifference);
+        results[actualGameStage] = timeDifference;
+        if (actualGameStage < gameStages) {
+            timeDifference = [];
+            cycles = 1;
+            gameState = "BREAK";
+            $('#message-container').html("Naciśnij spację, aby kontynuować.")
+            $('#counter-container').empty();
+        }
+        else {
+            $('#message-container').html("Koniec badania.")
+            $('#counter-container').empty();
+        }
+        if (actualGameStage > 1)
+            audioStop();
     }
-    else{
-       $('#moving-block').css("animation-play-state", "paused")
-        console.log(timeDifference); 
+
+}
+
+function audioStart(numberOfAudio) {
+    var audioPath;
+    switch (numberOfAudio) {
+        case 1:
+            audioPath = 'content/audio1.mp3';
+            break;
+        case 2:
+            audioPath = 'content/audio1.mp3';
+            break;
     }
+    if (audioPath) {
+        audio = new Audio(audioPath);
+        audio.play();
+    }
+}
+
+function audioStop() {
+    audio.pause();
+    audio.load();
+}
+
+function countTo(max) {
+    var counter = max;
+    counting = true;
+    $('#counter-container').html(max);
+    (function addDot() {
+        setTimeout(function () {
+            if (counter-- > 0) {
+                $('#counter-container').html(counter === 0 ? "START" : counter);
+                addDot();
+            }
+            else {
+                counting = false;
+                gameStart();
+            }
+        }, 1000);
+    })();
 }
